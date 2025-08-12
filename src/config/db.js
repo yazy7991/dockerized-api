923dbe36c6e44a768bfb16c98cc330a930b51c91
yazy7991 <jj3369155@gmail.com>
2025-08-12 15:15:32 +0100

    Testing the database connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,16 @@
 import pkg from 'pg';
 const {Pool} = pkg;
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 const pool = new Pool({
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.DBPORT
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT, 
 })
 
 pool.on("connect", ()=>{
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pool from "./config/db.js";
 
 dotenv.config();
 
@@ -10,12 +11,18 @@ const port = process.env.PORT || 3001;
 
 //Middleware
 
-app.use(express.json())
-app.use(cors)
+app.use(express.json());
+app.use(cors());
 
 //Routes 
 
 // Error handling Middleware
+
+//Testing POSTGRES Connection
+app.get("/", async(req,res) =>{
+    const result = await pool.query("SELECT current_database()");
+    res.send(`The database name is : ${result.rows[0].current_database}`)
+})
 
 // Server running
 app.listen(port,()=>{
