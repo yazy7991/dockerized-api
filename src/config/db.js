bf89527dcafb67fb5e99f478c48e87760b9803be
yazy7991 <jj3369155@gmail.com>
2025-08-12 13:32:48 +0100

    Setting up postgres connection

---
+++ src/config/db.js
@@ -0,0 +1,10 @@
+import pkg from 'pg';
+const {Pool} = pkg;
+
+const pool = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.DBPORT
+})
